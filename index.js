91b8cfdd71f759cd1f63e14cc73eca9a6caf5cd1
jirishus <jirishus@yahoo.com>
2015-04-26 10:19:24 -0700

    cors

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
+// handle CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req,res) {
   res.send('index info');
 });
